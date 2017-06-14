be8689dacd566b29d0a50a618d4ab43c4d7904bd
Fausto Brito <G30@Faustos-MacBook-Air.local>
2017-06-13 20:06:12 -0400

    button with options

--- alert_hero/app/components/Component1/component1.js
+++ alert_hero/app/components/Component1/component1.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 
-/*import Component1 from './app/components/Component1/component1';
-*/import Icon from 'react-native-vector-icons/Ionicons';
+import Icon from 'react-native-vector-icons/Ionicons';
 import ActionButton from 'react-native-circular-action-menu';
 
+//Circular button class, props and states
 
-export default class alert_hero extends Component {
+export default class component1 extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Alert Hero!!
+          Basic Example
         </Text>
-
         <ActionButton buttonColor="rgba(231,76,60,1)">
           <ActionButton.Item buttonColor='#9b59b6' onPress={() => console.log("notes tapped!")}>
             <Icon name="ios-create" style={styles.actionButtonIcon} />
           </ActionButton.Item>
-
           <ActionButton.Item buttonColor='#3498db'  onPress={() => {}}>
             <Icon name="ios-notifications-off" style={styles.actionButtonIcon} />
           </ActionButton.Item>
-
           <ActionButton.Item buttonColor='#1abc9c'  onPress={() => {}}>
             <Icon name="ios-done-all" style={styles.actionButtonIcon} />
           </ActionButton.Item>
@@ -48,9 +45,8 @@ const styles = StyleSheet.create({
     fontSize: 20,
     height: 22,
     color: 'white',
-    width: 22
   }
 });
 
 // skip this line if using Create React Native App
-AppRegistry.registerComponent('alert_hero', () => alert_hero);
+AppRegistry.registerComponent('Component1', () => component1);
--- alert_hero/index.ios.js
+++ alert_hero/index.ios.js
@@ -1,53 +1,18 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+
+import Component1 from './app/components/Component1/component1';
 
 export default class alert_hero extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View>
+         <Component1/>
       </View>
+     
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
+// skip this line if using Create React Native App
 AppRegistry.registerComponent('alert_hero', () => alert_hero);
