ee2baf5497b9b612bfd41dbcad593bd561d38634
wilcook <wil.cook.is@gmail.com>
2017-06-20 23:06:17 -0400

    textbox working, gradle added

---
+++ alert_hero/src/AlertHero.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import {
+	AppRegistry,
+	Text
+
+} from 'react-native';
+
+import Message from './src/components/Message';
+import Buttons from './src/components/Buttons'
+
+export default class alert_hero extends Component {
+  render() {
+    return (
+
+
+        <Message />
+
+    );
+  }
+}
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('alert_hero', () => alert_hero);
